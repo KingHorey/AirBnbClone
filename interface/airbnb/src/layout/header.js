a96a7946090a6c6da341a016961ced4409d66a12
AJ Iyanu <aderemijoseph@live.com>
2024-11-08 22:46:28 +0100

    [moved] images to source folder [added ] logo to the topbar

--- interface/airbnb/src/layout/header.js
+++ interface/airbnb/src/layout/header.js
@@ -1,11 +1,18 @@
 import Stack from 'react-bootstrap/Stack';
+import Airbnb_Logo from '../img/Airbnb_Logo.png'
 
 function Topbar() {
     return(
     <Stack direction='horizontal' gap={3}>
-        <div className='p-2'>Logo here</div>
-        <div className='p-2'>Search here</div>
-        <div className='p-2'>users account here</div>
+        <div className='p-2 mx-5'>
+            <img 
+                src={Airbnb_Logo}
+                alt='airbnb long logo'
+                width={192}
+                />
+        </div>
+        <div className='p-2 mx-auto'>Search here</div>
+        <div className='p-2 mx-5'>users account here</div>
     </Stack>
     )
 }
